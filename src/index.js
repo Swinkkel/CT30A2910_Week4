b470a00708270aa02338a92c81f9a2c01454fba9
Tomi Helin <tomi.helin@student.lut.fi>
2023-07-27 11:33:58 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -3,48 +3,41 @@ const searchShow = document.getElementById("input_show");
 const submitBtn = document.getElementById("submit_data");
 submitBtn.addEventListener("click", getShows);
 
-function addShow(title, summary, img_medium) {
-  let div1 = document.createElement("div");
-  div1.className = "show-data";
+async function getShows() {
+  const show = searchShow.value;
 
-  let img = document.createElement("img");
-  img.src = img_medium;
-
-  let div2 = document.createElement("div");
-  div2.className = "show-info";
-
-  const h1_element = document.createElement("h1");
-  const p_element = document.createElement("p");
-
-  div2.appendChild(h1_element);
-  div2.appendChild(p_element);
-
-  div1.appendChild(img);
-  div1.appendChild(div2);
-
-  document.getElementById("body").appendChild(div1);
-
-  /*
-    <div class="show-data">
-    <img src="[show image medium]"> 
-    <div class="show-info"> 
-        <h1>[Show title]</h1> 
-        <p>[Show summary]</p> 
-    </div> 
-</div>'
-*/
-  consol.log("test");
-}
-
-function getShows() {
-  fetchData(searchShow.value);
-}
-
-async function fetchData(show) {
   const url = "https://api.tvmaze.com/search/shows?q=" + show;
 
   const showsPromise = await fetch(url);
   const showsJSON = await showsPromise.json();
 
   console.log(showsJSON);
+
+  showsJSON.forEach((element) => {
+    let div1 = document.createElement("div");
+    div1.className = "show-data";
+
+    if (element.show.image) {
+      let image = document.createElement("img");
+      image.src = element.show.image.medium;
+
+      div1.appendChild(image);
+    }
+
+    let div2 = document.createElement("div");
+    div2.className = "show-info";
+
+    let title = document.createElement("h1");
+    title.innerText = element.show.name;
+
+    let summary = document.createElement("p");
+    summary.innerHTML = element.show.summary;
+
+    div2.appendChild(title);
+    div2.appendChild(summary);
+
+    div1.appendChild(div2);
+
+    document.body.appendChild(div1);
+  });
 }
